1a37cbfb42ec6bbb247f92c377214ef60a67ce95
swali <swali@152.23.55.13>
2019-08-28 11:46:53 -0400

    Initial commit. Defined classes and set up the absolute bare bones.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -4,11 +4,43 @@ import java.util.Scanner;
 
 public class A1Novice {
 
+	/** Main entrypoint. We need to take input of customer names
+	 * and their bought items. We need to output their names
+	 * (in a specific format) and their totals
+	 * 
+	 * @param args Unused
+	 */
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numCustomers;
 		
+		// How many customers do we have?
+		// Will control how many times we ask for customer data
+		numCustomers = scan.nextInt();
+		
+		for( int i = 0; i < numCustomers; ++i ) {
+			
+		}
+	}
+	
+	/** A customer can be kept up in this object.
+	 * Each customer has a name, a number of items they bought
+	 * and the items themselves (name and cost)
+	 * 
+	 * @author swali
+	 *
+	 */
+	private class CustomerData {
+		
+		/** An item is simply a name and the cost, nothing more.
+		 * 
+		 * @author swali
+		 *
+		 */
+		private class Item {
+		}
 	}
 }
